d8688c60b019fd757f258367e82a9d13514c66bc
Hudson Reis <hudsonpr09@gmail.com>
2025-11-28 13:37:46 -0300

    fix: problemas de style shift

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,3 @@
-// Adicionando Fonte
-import { Nunito } from "next/font/google";
-
 // Providers
 import MyThemeProvider from "../providers/themeProvider";
 import ReduxProvider from "../providers/reduxProvider";
@@ -8,12 +5,16 @@ import ReduxProvider from "../providers/reduxProvider";
 // Garante a Sincronização cliente/servidor do theme
 import ThemeHydrator from "@/components/ThemeHydrator";
 
+// Adicionando Fonte
+import { Nunito } from "next/font/google";
+import StyledComponentsRegistry from "../../lib/registry";
 const nunito = Nunito({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-nunito",
 });
 
+// Metadados
 export const metadata = {
   title: "Desafio Técnico | Lacrei Saúde",
 };
@@ -24,13 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR" className={nunito.variable}>
-      <body>
+    <html lang="pt-BR" className={nunito.className}>
+      <body suppressHydrationWarning={true}>
         <ReduxProvider>
-          <MyThemeProvider>
-            <ThemeHydrator />
-            {children}
-          </MyThemeProvider>
+          <StyledComponentsRegistry>
+            <MyThemeProvider>
+              <ThemeHydrator />
+              {children}
+            </MyThemeProvider>
+          </StyledComponentsRegistry>
         </ReduxProvider>
       </body>
     </html>
